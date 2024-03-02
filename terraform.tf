c381f4763dbfdbeeb509f3846815186abc3018c8
Muzammil Pasha <156335256+muzammilpasha4@users.noreply.github.com>
2024-03-02 16:41:13 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,3 @@
-#Initialize Terraform
 terraform {
   required_providers {
     google = {
@@ -8,141 +7,74 @@ terraform {
   }
 }
 
-# Configure the AWS provider
 provider "google" {
-  region = "us-east-1"
+  credentials = file("gcp-key.pem")
+  project     = "elevated-style-415906"  
+  region      = "us-central1"
 }
-# Creating a VPC
+
 resource "google_compute_network" "first-vpc" {
- cidr_block = "10.0.0.0/16"
+  name                    = "first-vpc"
+  auto_create_subnetworks = false
 }
 
-# Create an Internet Gateway
-resource "google_compute_route" "proj-ig" {
- vpc_id = google_compute_network.first-vpc.id
- tags = {
- Name = "gateway1"
- }
+resource "google_compute_global_address" "proj-eip" {
+  name                  = "proj-eip"
+  address_type          = "EXTERNAL"
+  purpose               = "GCE_ENDPOINT"
+  prefix_length         = 32
+  project               = "elevated-style-415906"
 }
 
-# Setting up the route table
-resource "google_compute_route" "proj-rt" {
- vpc_id = google_compute_network.first-vpc.id
- route {
- # pointing to the internet
- cidr_block = "0.0.0.0/0"
- gateway_id = google_compute_route.proj-ig.id
- }
- route {
- ipv6_cidr_block = "::/0"
- gateway_id = google_compute_route.proj-ig.id
- }
- tags = {
- Name = "rt1"
- }
-}
-
-# Setting up the subnet
 resource "google_compute_subnetwork" "proj-subnet" {
- vpc_id = google_compute_network.first-vpc.id
- cidr_block = "10.0.1.0/24"
- availability_zone = "us-east-1b"
- tags = {
- Name = "subnet1"
- }
+  name          = "proj-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.first-vpc.self_link
 }
 
-# Associating the subnet with the route table
-resource "google_compute_route" "proj-rt-sub-assoc" {
-subnet_id = google_compute_subnetwork.proj-subnet.id
-route_table_id = google_compute_route.proj-rt.id
-}
+resource "google_compute_firewall" "proj-sg" {
+  name    = "proj-sg"
+  network = google_compute_network.first-vpc.name
 
-# Creating a Security Group
-resource "google_compute_security_policy" "proj-sg" {
- name = "proj-sg"
- description = "Enable web traffic for the project"
- vpc_id = google_compute_network.first-vpc.id
- ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443","22","8085"]
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  allow {
+    protocol = "icmp"
   }
- ingress {
- description = "HTTPS traffic"
- from_port = 443
- to_port = 443
- protocol = "tcp"
- cidr_blocks = ["0.0.0.0/0"]
- }
- ingress {
- description = "HTTP traffic"
- from_port = 0
- to_port = 65000
- protocol = "tcp"
- cidr_blocks = ["0.0.0.0/0"]
- }
- ingress {
- description = "Allow port 80 inbound"
- from_port   = 80
- to_port     = 80
- protocol    = "tcp"
- cidr_blocks = ["0.0.0.0/0"]
-  }
- egress {
- from_port = 0
- to_port = 0
- protocol = "-1"
- cidr_blocks = ["0.0.0.0/0"]
- ipv6_cidr_blocks = ["::/0"]
- }
- tags = {
- Name = "proj-sg1"
- }
+
+  source_ranges = ["0.0.0.0/0"]
 }
 
-# Creating a new network interface
-resource "google_compute_network_interface" "proj-ni" {
- subnet_id = google_compute_network_subnet.proj-subnet.id
- private_ips = ["10.0.1.10"]
- security_groups = [google_compute_network_security_group.proj-sg.id]
-}
-
-# Attaching an elastic IP to the network interface
-resource "google_compute_address" "proj-eip" {
- vpc = true
- network_interface = google_compute_network_interface.proj-ni.id
- associate_with_private_ip = "10.0.1.10"
-}
-
-
-# Creating an ubuntu EC2 instance
 resource "google_compute_instance" "Test-Server" {
- ami = "elevated-style-415906"
- instance_type = "t2.micro"
- availability_zone = "us-east-1b"
- key_name = "gcp-key.pem"
- network_interface {
- device_index = 0
- network_interface_id = google_compute_network_interface.proj-ni.id
- }
- user_data  = <<-EOF
- #!/bin/bash
-     sudo apt-get update -y
-     sudo apt install docker.io -y
-     sudo systemctl enable docker
-     sudo docker run -itd -p 8085:8082 muzammilp/medicureimgtf8082:latest
-     sudo docker start $(docker ps -aq)
- EOF
- tags = {
- Name = "Test-Server"
- }
+  name         = "Test-Server"
+  machine_type = "e2-micro"
+  zone         = "us-east4-a"
+  tags         = ["http-server", "https-server"]
+
+  boot_disk {
+    initialize_params {
+      image = "projects/ubuntu-os-cloud/global/images/family/ubuntu-22.04-lts"
+    }
+  }
+
+  network_interface {
+    network       = google_compute_network.first-vpc.self_link
+    subnetwork    = google_compute_subnetwork.proj-subnet.self_link
+    access_config {
+      nat_ip = google_compute_global_address.proj-eip.address
+    }
+  }
+
+  metadata_startup_script = <<-EOF
+    #!/bin/bash
+    sudo apt-get update -y
+    sudo apt install docker.io -y
+    sudo systemctl enable docker
+    sudo docker run -itd -p 8085:8082 muzammilp/insuranceimgaddbook:latest
+    sudo docker start $(docker ps -aq)
+  EOF
 }
