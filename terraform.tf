ae6b15d21b54a5af5e4fe9680c10ef5b25f7c76d
Muzammil Pasha <156335256+muzammilpasha4@users.noreply.github.com>
2024-03-02 15:34:04 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -126,7 +126,7 @@ resource "aws_eip" "proj-eip" {
 
 # Creating an ubuntu EC2 instance
 resource "aws_instance" "Test-Server" {
- project_id = "elevated-style-415906"
+ ami = "elevated-style-415906"
  instance_type = "t2.micro"
  availability_zone = "us-east-1b"
  key_name = "gcp-key.pem"
